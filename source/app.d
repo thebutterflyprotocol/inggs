f2c18e4645fd7e5f44b4f7abab895b91165d7722
Tristan B. Kildaire <deavmi@disroot.org>
2020-07-21 17:27:52 +0200

    Server setup.

--- source/app.d
+++ source/app.d
@@ -6,11 +6,17 @@ void main()
 {
 	writeln("Starting Inggs - Butterfly webmail server");
 
+	/* Create a router */
 	URLRouter router = new URLRouter();
-
 	router.get("/", &login);
 
-	/* TODO: Attach router */
+	/* Setup the HTTP server */
+	HTTPServerSettings serverSettings;
+	serverSettings.port = 8080;
+	serverSettings.bindAddresses = ["0.0.0.0", "::"];
+
+	/* Attach the HTTP server's settings and the route handler */
+	listenHTTP(serverSettings, router);
 
 	writeln("Starting vibe event loop...");
 	runApplication();

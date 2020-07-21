b8eb3cb18c607b9316e04159a3eb0df57871ced1
Tristan B. Kildaire <deavmi@disroot.org>
2020-07-21 18:05:54 +0200

    FIxed compilation error

--- source/views.d
+++ source/views.d
@@ -2,5 +2,5 @@ import vibe.vibe;
 
 void login(HTTPServerRequest req, HTTPServerResponse resp)
 {
-    resp.writeBody(cast(ubyte)"Welcome to Inggs webmail");
+    resp.writeBody(cast(ubyte[])"Welcome to Inggs webmail");
 }

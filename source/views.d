a4eefe002a3f4a85b8166a41d1c48c936372ed1b
Tristan B. Kildaire <deavmi@disroot.org>
2020-07-27 11:03:55 +0200

    Set content type

--- source/views.d
+++ source/views.d
@@ -2,5 +2,6 @@ import vibe.vibe;
 
 void login(HTTPServerRequest req, HTTPServerResponse resp)
 {
+    resp.headers["Content-Type"] = "text/plain";
     resp.writeBody(cast(ubyte[])"Welcome to Inggs webmail");
 }
